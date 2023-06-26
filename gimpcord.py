212dc6fb5131b68a7df4a7beb92ded4356eb9ac2
DOODER(Michael Smith) <40189403+dooder-codes@users.noreply.github.com>
2023-06-25 23:25:45 -0400

    Update gimpcord.py

--- gimpcord.py
+++ gimpcord.py
@@ -1,43 +1,47 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import datetime
 import sys, traceback, trace
 import os, io, time
 from gimpfu import *
 import discord_rpc
+
 pdb = gimp.pdb
 
 class HandleErrors:
     def __init__(self):
         self.exc_type, self.exc_value, self.exc_traceback = sys.exc_info()
-
     def throw_GIMP_traceback(self):
-        error_message = "".join(traceback.format_exception(self.exc_type, self.exc_value, self.exc_traceback))
+        error_message = ' '.join( traceback.format_exception( self.exc_type, self.exc_value, self.exc_traceback ))
         pdb.gimp_message(error_message)
 
 class Echo:
     def __init__(self):
         pass
-    def echo(self,param):
-        pdb.gimp_message(param)
-    def trace(self,msg):
-        lineNumber = sys._getframe().f_lineno
+    def echo( self, *args ):
+        text = ' '.join(map( str, args ))
+        pdb.gimp_message(text)
+    def trace( self, *args ):
+        line_number = sys._getframe(1).f_lineno
         cwd = os.getcwd()
-        file = os.path.join(cwd,'gimpcord.py')
-        pdb.gimp_message('{}\r\nLine: #{}\r\nFile: {}\r\n'.format(msg,lineNumber,file))
-
+        file = os.path.join( cwd, 'gimpcord.py' )
+        text = ' '.join( map( str, args ))
+        pdb.gimp_message('{}\r\nLine: #{}\r\nFile: {}\r\n'
+                        .format( text, line_number,file ))
 
 today = datetime.date.today() # Get today's date
-formatted_date = today.strftime("%d/%m/%Y") # Format the date as DD/MM/YYYY
+formatted_date = today.strftime( "%d/%m/%Y" ) # Format the date as DD/MM/YYYY
 echo = Echo()
 
-def readyCallback(current_user):
+def readyCallback( current_user ):
     print('Our user: {}'.format( current_user ))
 
-def disconnectedCallback(codeno, codemsg):
-    print('Disconnected from Discord rich presence RPC. Code {}: {}'.format( codeno, codemsg ))
+def disconnectedCallback( codeno, codemsg ):
+    print('Disconnected from Discord rich presence RPC. Code {}: {}'
+        .format( codeno, codemsg ))
 
-def errorCallback(errno, errmsg):
+def errorCallback( errno, errmsg ):
     print('An error occurred! Error {}: {}'.format( errno, errmsg ))
 
 # Note: 'event_name': callback
@@ -48,33 +52,40 @@ callbacks = {
     }
 
 def load_client_id():
-        dotenv_path = os.path.abspath("/plug-ins/Dooder/gimpcord/.env")
-        client_id = os.getenv('client_id')
+        dotenv_file = os.path.abspath( '/plug-ins/Dooder/gimpcord/.env' )
+        client_id = os.getenv( 'client_id' )
+
         if not client_id:
-            echo.trace("client_id = Null")
-            raise ValueError('Missing client ID in .env file')
+            echo.trace( 'Missing client ID in .env file' )
+
+        echo.trace( 'client ID: {}'.format(client_id) )
         return client_id
 
 def init_discord_rpc(image,client_id):
-    discord_rpc.initialize(client_id, callbacks=callbacks, log=False)
-    pdb.gimp_message("Connected to Discord RPC")
+    if client_id != None:
+        echo.echo("Connected to Discord RPC")
+        discord_rpc.initialize( client_id, callbacks=callbacks, log=False )
 
-    start = time.time()
-    fileName = image.filename
-    active = image.active_layer
-    width = image.width
-    height = image.height
-    layers = len(image.layers)
+        start = time.time()
+        file_name = image.filename
+        active = image.active_layer
+        width = image.width
+        height = image.height
+        layers = len(image.layers)
 
-    while True:
-        discord_rpc.update_presence(
-            details=fileName,
-            state='Resolution: {}x{} | Layers: {}'.format(width,height,layers),
-            start='{}'.format(start),
-            large_image='default')
-        discord_rpc.update_connection()
-        time.sleep(2)
-        discord_rpc.run_callbacks()
+        while True:
+            discord_rpc.update_presence(
+                details=file_name,
+                state='Resolution: {}x{} | Layers: {}'
+                .format( width, height, layers ),
+                start='{}'.format(start),
+                large_image='default' )
+            discord_rpc.update_connection()
+            time.sleep(2)
+            discord_rpc.run_callbacks()
+    else:
+        echo.echo("Not connected to Discord")
+
 
 # discord_rpc.shutdown()
 def gimpcord(image, drawable):
