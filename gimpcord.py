0b1b21a97196b5ab612eb2186a9fb35db1150cf1
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-07-04 20:49:17 -0400

    fixes involving updating status with correct stats

--- gimpcord.py
+++ gimpcord.py
@@ -5,20 +5,21 @@ import datetime
 import sys, traceback, trace
 import os, io, time
 from gimpfu import *
+import gimp
 import discord_rpc
 
 pdb = gimp.pdb
 today = datetime.date.today() # Get today's date
 formatted_date = today.strftime( "%d/%m/%Y" ) # Format the date as DD/MM/YYYY
-whoiam="\n"+os.path.abspath(sys.argv[0])
-plugin_name="drp"
-author="Dooder"
-where="<Image>"
-menu="/Filters/Discord/drp"
-menu_path=where+menu
+whoiam = "\n"+os.path.abspath(sys.argv[0])
+plugin_name = "drp"
+author = "Dooder"
+where = "<Image>"
+menu = "/Filters/Discord/drp"
+menu_path = where + menu
 path_to_config = 'C:\Users\User\AppData\Roaming\GIMP\\2.10\plug-ins\Dooder\gimpcord\config.txt'
-date_of_creation=formatted_date
-desc=""
+date_of_creation = formatted_date
+desc = ""
 
 class HandleErrors:
     def __init__(self):
@@ -47,6 +48,7 @@ class Echo:
         file = os.path.join( cwd, 'gimpcord.py' )
         pdb.gimp_message('{}\r\nLine: #{}\r\nFile: {}\r\n'
                         .format( text, line_number,file ))
+
 echo = Echo()
 
 def readyCallback( current_user ):
@@ -80,11 +82,12 @@ def setClientID(clientID):
     """
     Update the clientID in config.txt
     """
-    lines = readConfigFile()
-    lines[0][1] = clientID
+    read_config = readConfigFile()
+    # updates the second element ([1]) of the first line ([0])
+    read_config[0][1] = clientID
 
     with open(path_to_config, "w") as file:
-        for line in lines:
+        for line in read_config:
             file.write(":".join(line) + "\n")
 
 def getClientID():
@@ -105,28 +108,36 @@ def initDiscordRPC(image, drawable, clientID):
         echo.echo("Client ID is empty or incorrect.\r\nFailed to connect to Discord.\r\n\r\nPlease reactivate the plug-in at " + menu + " and try again.\r\nAlternatively, you can re-show the last used filter by using the default keybinding 'Shift+Ctrl+F' to try entering in your ID again.")
 
     else:
-        echo.echo("Connected to Discord RPC")
+        echo.echo("Connected to Discord RPC.")
+
         discord_rpc.initialize( clientID, callbacks=callbacks, log=False )
         setClientID(clientID)
 
         start = time.time()
-        file_name = image.filename
-        active = image.active_layer
-        width = image.width
-        height = image.height
-        layers = len(image.layers)
+        file_name = ""
 
         while True:
+            image = gimp.image_list()[0]  # Get the active image
+            width = image.width
+            height = image.height
+            layers = len(image.layers)
+            # active = image.active_layer
+
+            if image.filename is None:
+                file_name = "[Untitled]"
+            else:
+                file_name = image.filename
+
             discord_rpc.update_presence(
-                details=file_name,
-                state='Resolution: {}x{} | Layers: {}'
+                details = file_name,
+                state = '{}x{} | Layers: {}'
                 .format( width, height, layers ),
-                start='{}'.format(start),
-                large_image='default' )
+                start = '{}'.format(start),
+                large_image_key = "gimp_logo"
+                )
             discord_rpc.update_connection()
             time.sleep(2)
             discord_rpc.run_callbacks()
-
 # discord_rpc.shutdown()
 
 def gimpcord(image, drawable, clientID):
@@ -149,7 +160,7 @@ def register_plugin():
         menu_path,
         "*",
         [
-                (PF_STRING, "text", "ClientID: ", " "),
+                (PF_STRING, "text", "ClientID: ", getClientID()),
         ],
         [],  # No return values
         gimpcord, # Your plugin function
